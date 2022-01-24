eefc9a87e6a483d4db82f2851b79132ce20fc33d
Yulia51188 <sun51188@gmail.com>
2022-01-24 13:15:46 +0300

    Extend data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.templatetags.static import static
 from phonenumber_field.phonenumber import PhoneNumber
+import phonenumbers
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -67,9 +68,8 @@ def product_list_api(request):
 def register_order(request):
     order_details = request.data
 
-    is_valid_products, error_details = validate_products(
-        order_details.get('products'))
-    if not is_valid_products:
+    is_valid_post_data, error_details = validate_post_data(order_details)
+    if not is_valid_post_data:
         return Response(error_details, status=status.HTTP_400_BAD_REQUEST)
 
     order = Order.objects.create(
@@ -95,8 +95,82 @@ def register_order(request):
     return Response(order_details)
 
 
-def validate_products(products):
-    if products is None:
+def validate_post_data(order_details):
+    firstname = order_details.get('firstname')
+    if not firstname:
+        return False, {
+            "type": "ValueError",
+            "data_filed": "firstname",
+            "message": "firstname field value is empty or skipped",
+        }
+    if not isinstance(firstname, str):
+        return False, {
+            "type": "TypeError",
+            "data_filed": "firstname",
+            "message": "firstname field value is not a string",
+        }
+
+    lastname = order_details.get('lastname')
+    if not lastname:
+        return False, {
+            "type": "ValueError",
+            "data_filed": "lastname",
+            "message": "lastname field value is empty or skipped",
+        }
+    if not isinstance(lastname, str):
+        return False, {
+            "type": "TypeError",
+            "data_filed": "lastname",
+            "message": "lastname field value is not a string",
+        }
+
+    address = order_details.get('lastname')
+    if not address:
+        return False, {
+            "type": "ValueError",
+            "data_filed": "address",
+            "message": "address field value is empty or skipped",
+        }
+    if not isinstance(address, str):
+        return False, {
+            "type": "TypeError",
+            "data_filed": "address",
+            "message": "address field value is not a string",
+        }
+
+    phonenumber = order_details.get('phonenumber')
+    print('PHONE NUMBER', phonenumber)
+    if not phonenumber:
+        return False, {
+            "type": "ValueError",
+            "data_filed": "phonenumber",
+            "message": "phonenumber field value is empty or skipped",
+        }
+    if not isinstance(phonenumber, str):
+        return False, {
+            "type": "TypeError",
+            "data_filed": "phonenumber",
+            "message": "phonenumber field value is not a string",
+        }
+    try:
+        parsed_phone = phonenumbers.parse(phonenumber, "RU")
+        is_valid = phonenumbers.is_valid_number(parsed_phone)
+        is_possible = phonenumbers.is_possible_number(parsed_phone)
+        if not is_valid or not is_possible:
+            return False, {
+                "type": "ValueError",
+                "data_filed": "phonenumber",
+                "message": "phonenumber field value is invalid for RU region",
+            }
+    except phonenumbers.phonenumberutil.NumberParseException as error:
+        return False, {
+            "type": "ValueError",
+            "data_filed": "phonenumber",
+            "message": f"phonenumber field value is invalid: {error}",
+        }
+
+    products = order_details.get('products')
+    if not products:
         return False, {
             "type": "ValueError",
             "data_filed": "products",
@@ -104,7 +178,7 @@ def validate_products(products):
         }
     if not isinstance(products, list):
         return False, {
-            "type": "ValueError",
+            "type": "TypeError",
             "data_filed": "products",
             "message": "products field value is not a list",
         }
