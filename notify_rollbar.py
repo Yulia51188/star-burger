5cf232afc81250c40ed6140e81fee735be8bf60d
Yulia51188 <sun51188@gmail.com>
2022-04-11 14:42:57 +0300

    Remove Django import from notifier

--- notify_rollbar.py
+++ notify_rollbar.py
@@ -1,19 +1,28 @@
 import requests
 import subprocess
-from django.conf import settings
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "star_burger.settings")
+from environs import Env
 
-# fetch last committed revision in the locally-checked out branch
-git_process = subprocess.run(['git', 'log', '-n', '1', '--pretty=format:%H'], capture_output=True, check=True)
+env = Env()
+env.read_env()
+
+git_process = subprocess.run(
+    ['git', 'log', '-n', '1', '--pretty=format:%H'],
+    capture_output=True,
+    check=True
+)
 revision = git_process.stdout.decode()
 
-response = requests.post('https://api.rollbar.com/api/1/deploy/', json={
-    'environment': settings.ROLLBAR_ENV_LABEL,
-    'revision': revision,
-}, headers={
-    'X-Rollbar-Access-Token': settings.ROLLBAR_TOKEN,
-}, timeout=3)
+response = requests.post(
+    'https://api.rollbar.com/api/1/deploy/',
+    json={
+        'environment': env('ROLLBAR_ENV_LABEL', 'production'),
+        'revision': revision,
+    },
+    headers={
+        'X-Rollbar-Access-Token': env('ROLLBAR_TOKEN'),
+    },
+    timeout=3
+)
 
 response.raise_for_status()
